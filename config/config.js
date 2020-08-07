b40122cfd9012a0f02c4ad56c8295a6964488442
arnihl <arnihl@gmail.com>
2020-08-07 15:28:07 +0000

    testnet fix

--- config/config.js
+++ config/config.js
@@ -61,8 +61,8 @@ var ignoreCache = process.env.INSIGHT_IGNORE_CACHE || 0;
 
 var bitcoindConf = {
   protocol: process.env.BITCOIND_PROTO || 'http',
-  user: process.env.BITCOIND_USER || 'smileycoinrpc',
-  pass: process.env.BITCOIND_PASS || '5jdW1qBQLbVznUYj9JRdcpwxcDptQuLSfSr4r3MigKr4',
+  user: process.env.BITCOIND_USER || 'user',
+  pass: process.env.BITCOIND_PASS || '',
   host: process.env.BITCOIND_HOST || '127.0.0.1',
   port: process.env.BITCOIND_PORT || b_port,
   p2pPort: process.env.BITCOIND_P2P_PORT || p2p_port,
